27c44ec225ac84b095bb91c693ad3f4b956fa130
Craig <Craig@MacBook-Pro.local>
2026-04-16 12:28:53 -0600

    Quoting out old code for now

--- test/pageobjects/menu.page.js
+++ test/pageobjects/menu.page.js
@@ -1,57 +1,57 @@
-import Page from './page.js';
+// import Page from './page.js';
 
 
-class MenuPage extends Page {
-    get burgerBtn() {
-        return $('#react-burger-menu-btn');
-    }
+// class MenuPage extends Page {
+//     get burgerBtn() {
+//         return $('#react-burger-menu-btn');
+//     }
 
-    get closeBtn() {
-        return $('#react-burger-cross-btn');
-    }
+//     get closeBtn() {
+//         return $('#react-burger-cross-btn');
+//     }
 
-    get allItemsLnk() {
-        return $('//a[@id="inventory_sidebar_link"][text()="All Items"]');
-    }
+//     get allItemsLnk() {
+//         return $('//a[@id="inventory_sidebar_link"][text()="All Items"]');
+//     }
 
-    get aboutLnk() {
-        return $('//a[@id="about_sidebar_link"][text()="About"]');
-    }
+//     get aboutLnk() {
+//         return $('//a[@id="about_sidebar_link"][text()="About"]');
+//     }
 
-    get logoutLnk() {
-        return $('//a[@id="logout_sidebar_link"][text()="Logout"]');
-    }
+//     get logoutLnk() {
+//         return $('//a[@id="logout_sidebar_link"][text()="Logout"]');
+//     }
 
-    get resetLnk() {
-        return $('//a[@id="reset_sidebar_link"][text()="Reset App State"]');
-    }
+//     get resetLnk() {
+//         return $('//a[@id="reset_sidebar_link"][text()="Reset App State"]');
+//     }
 
-    async openMenu() {
-        await this.burgerBtn.click();
-        await this.closeBtn.waitForDisplayed();
-    }
+//     async openMenu() {
+//         await this.burgerBtn.click();
+//         await this.closeBtn.waitForDisplayed();
+//     }
 
-    async closeMenu() {
-        await this.closeBtn.click();
-        await this.burgerBtn.waitForDisplayed();
-    }
+//     async closeMenu() {
+//         await this.closeBtn.click();
+//         await this.burgerBtn.waitForDisplayed();
+//     }
 
-    async logout() {
-        await this.logoutLink.click();
-    }
+//     async logout() {
+//         await this.logoutLink.click();
+//     }
 
        
-    async checkMenuLinks() {
-        await expect(this.allItemsLnk).toBeDisplayed();
-        await expect(this.aboutLnk).toBeDisplayed();
-        await expect(this.logoutLnk).toBeDisplayed();
-        await expect(this.resetLnk).toBeDisplayed();
-    }
+//     async checkMenuLinks() {
+//         await expect(this.allItemsLnk).toBeDisplayed();
+//         await expect(this.aboutLnk).toBeDisplayed();
+//         await expect(this.logoutLnk).toBeDisplayed();
+//         await expect(this.resetLnk).toBeDisplayed();
+//     }
 
         
 
     
         
-}
+// }
 
-   export default new MenuPage(); 
+//    export default new MenuPage(); 
